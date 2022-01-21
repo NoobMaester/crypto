c8bf3002655e05d58c4e56be328e18c6034c0bc1
NoobMaester <joshstilez@gmail.com>
2022-01-21 09:52:51 +0200

    alt prop to img

--- src/component/Coin.js
+++ src/component/Coin.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Coin = ({name, logo, price, symbol}) => {
   return <div className='coin'>
       <h1>Name: {name}</h1>
-      <img src={logo}/>
+      <img src={logo} alt="icon" />
       <h3>Price: {price}</h3>
       <h3>Symbol: {symbol}</h3>
   </div>;
